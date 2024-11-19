05dd697160fcbdc06fdd6f8fda3ae32a04714572
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-19 16:53:09 +0100

    areas add

---
+++ Project02BurgerMenu/Content/BurgerMenu01Context.cs
@@ -0,0 +1,17 @@
+﻿using Project02BurgerMenu.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project02BurgerMenu.Content
+{
+    public class BurgerMenu01Context :DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<DealOfTheDay> DealOfTheDays { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
+        public DbSet<Product> Products { get; set; }    
+    }
+}
