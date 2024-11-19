6f856ecb6efd9e44713892b7d0452d89663e675b
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-19 18:47:08 +0100

    product added

---
+++ Project02BurgerMenu/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,40 @@
+﻿using Project02BurgerMenu.Content;
+using Project02BurgerMenu.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project02BurgerMenu.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        BurgerMenu01Context db = new BurgerMenu01Context();
+        public ActionResult ProductList()
+        {
+            var productList = db.Products.ToList();
+            return View(productList);
+        }
+        [HttpGet]
+        public ActionResult CreateProduct()
+        {
+            List<SelectListItem> values = (from x in db.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v1 = values;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+    }
+}
