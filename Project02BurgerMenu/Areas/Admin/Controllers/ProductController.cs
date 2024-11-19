71245fd08e502b7785b1de17d66f095f9ead9aa7
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-19 19:00:03 +0100

    update product

--- Project02BurgerMenu/Areas/Admin/Controllers/ProductController.cs
+++ Project02BurgerMenu/Areas/Admin/Controllers/ProductController.cs
@@ -36,5 +36,39 @@ namespace Project02BurgerMenu.Areas.Admin.Controllers
             return RedirectToAction("ProductList");
         }
 
+        public ActionResult DeleteProduct(int id)
+        {
+            var product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateProduct(int id)
+        {
+            var product = db.Products.Find(id);
+            List<SelectListItem> values = (from x in db.Categories.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = x.CategoryName,
+                                               Value = x.CategoryId.ToString()
+                                           }).ToList();
+            ViewBag.v1 = values;
+            return View("EditProduct", product);
+        }
+        [HttpPost]
+        public ActionResult UpdateProduct(Product product)
+        {
+            var p = db.Products.Find(product.ProductId);
+            p.ProductName = product.ProductName;
+            p.ProductPrice = product.ProductPrice;
+            p.ProductDescription = product.ProductDescription;
+            p.ImageUrl = product.ImageUrl;
+            p.CategoryId = product.CategoryId;
+            db.SaveChanges();
+            return RedirectToAction("ProductList");
+        }
+
     }
 }
