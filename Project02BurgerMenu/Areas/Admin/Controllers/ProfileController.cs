83d87f73c0437e2ea8e2f118e827a1cb0d2c5c1e
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-21 13:15:44 +0100

    BurgerMenu Profilelist added

---
+++ Project02BurgerMenu/Areas/Admin/Controllers/ProfileController.cs
@@ -0,0 +1,35 @@
+﻿using Project02BurgerMenu.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project02BurgerMenu.Entities;
+
+namespace Project02BurgerMenu.Areas.Admin.Controllers
+{
+    public class ProfileController : Controller
+    {
+        BurgerMenu01Context context = new BurgerMenu01Context();
+        [HttpGet]
+        public ActionResult MyProfileList()
+        {
+            var userName = Session["x"];
+            var values = context.Admins.Where(x => x.Username == userName).FirstOrDefault();
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult MyProfileList(Project02BurgerMenu.Entities.Admin admin)
+        {
+            var userName = Session["x"];
+            var x = context.Admins.Where(y => y.Username == userName).FirstOrDefault();
+            x.Name = admin.Name;
+            x.Surname = admin.Surname;
+            x.Password = admin.Password;
+            x.Username = admin.Username;
+            x.Email = admin.Email;
+            context.SaveChanges();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
