d148d444319b7b2c2e4b952d89c0c53e66ac684e
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-22 10:30:46 +0100

    message added

---
+++ Project02BurgerMenu/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project02BurgerMenu.Entities;
+using Project02BurgerMenu.Content;
+
+namespace Project02BurgerMenu.Areas.Admin.Controllers
+{
+    public class MessageController : Controller
+    {
+        BurgerMenu01Context db = new BurgerMenu01Context();
+        public ActionResult InBox()
+        {
+            var userName = Session["x"];
+            var email = db.Admins.Where(x => x.Username == userName).Select(x => x.Email).FirstOrDefault();
+            var messages = db.Messages.Where(x => x.ReceiverEmail == email).ToList();
+            return View(messages);
+        }
+        public ActionResult SendBox()
+        {
+            var userName = Session["x"];
+            var email = db.Admins.Where(x => x.Username == userName).Select(x => x.Email).FirstOrDefault();
+            var messages = db.Messages.Where(x => x.SenderEmail == email).ToList();
+            return View(messages);
+        }
+        [HttpGet]
+        public ActionResult NewMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NewMessage(Message message)
+        {
+            message.IsRead = false;
+            message.SendDate = DateTime.Now;
+            db.Messages.Add(message);
+            db.SaveChanges();
+            return RedirectToAction("SendBox", new { area = "Admin" });
+        }
+    }
+}
--- Project02BurgerMenu/Content/BurgerMenu01Context.cs
+++ Project02BurgerMenu/Content/BurgerMenu01Context.cs
@@ -16,5 +16,6 @@ namespace Project02BurgerMenu.Content
 
         public DbSet<Admin> Admins { get; set; }
         public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 }
