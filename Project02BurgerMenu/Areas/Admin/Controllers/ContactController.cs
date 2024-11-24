df9b0b4b1a737526d83e5f19a32c944359c04a18
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-24 12:34:28 +0100

    message details added

--- Project02BurgerMenu/Areas/Admin/Controllers/ContactController.cs
+++ Project02BurgerMenu/Areas/Admin/Controllers/ContactController.cs
@@ -10,11 +10,39 @@ namespace Project02BurgerMenu.Areas.Admin.Controllers
 {
     public class ContactController : Controller
     {
-        BurgerMenu01Context context = new BurgerMenu01Context();    
+        BurgerMenu01Context context = new BurgerMenu01Context();
         public ActionResult Inbox()
         {
-            var values = context.Contacts.ToList(); 
+            var values = context.Contacts.ToList();
             return View(values);
+        }
+        public ActionResult DetailMessage(int id)
+        {
+            var value = context.Contacts.Where(x => x.ContactID == id).FirstOrDefault();
+            value.IsRead = true;
+            context.SaveChanges();
+            return View(value);
+        }
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value = context.Contacts.Where(x => x.ContactID == id).FirstOrDefault();
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value = context.Contacts.Where(x => x.ContactID == id).FirstOrDefault();
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+        }
+        public ActionResult DeleteMessage(int id)
+        {
+            var values = context.Contacts.Find(id);
+            context.Contacts.Remove(values);
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
         }
     }
 }
