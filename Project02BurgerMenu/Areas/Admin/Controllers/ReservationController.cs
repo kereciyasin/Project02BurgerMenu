9fca238e078d0ac7aef8c5a9f3038cf83cf05cc8
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-26 10:02:04 +0100

    reservation list added

---
+++ Project02BurgerMenu/Areas/Admin/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project02BurgerMenu.Content;
+using Project02BurgerMenu.Entities;
+using PagedList;
+using PagedList.Mvc;
+
+namespace Project02BurgerMenu.Areas.Admin.Controllers
+{
+    public class ReservationController : Controller
+    {
+        BurgerMenu01Context context = new BurgerMenu01Context();
+        public ActionResult ReservationList(int page = 1)
+        {
+            var values = context.Reservations.ToList().ToPagedList(page, 5);   
+            return View(values);
+        }
+    }
+}
