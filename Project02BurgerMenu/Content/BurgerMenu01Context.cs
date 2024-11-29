ae85a4deb54678f9f8caa61f1d845ba1f3201271
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-30 00:31:21 +0100

    migration added

--- Project02BurgerMenu/Content/BurgerMenu01Context.cs
+++ Project02BurgerMenu/Content/BurgerMenu01Context.cs
@@ -19,5 +19,6 @@ namespace Project02BurgerMenu.Content
         public DbSet<Message> Messages { get; set; }
         public DbSet<About> Abouts { get; set; }
         public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Subscribe> Subscribes { get; set; }
     }
 }
---
+++ Project02BurgerMenu/Migrations/202411292330518_subs.cs
@@ -0,0 +1,26 @@
+﻿namespace Project02BurgerMenu.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class subs : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Subscribes",
+                c => new
+                    {
+                        SubscribeId = c.Int(nullable: false, identity: true),
+                        Email = c.String(nullable: false),
+                    })
+                .PrimaryKey(t => t.SubscribeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Subscribes");
+        }
+    }
+}
