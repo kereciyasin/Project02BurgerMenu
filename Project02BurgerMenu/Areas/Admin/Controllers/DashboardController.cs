2ba3e42ab813234308d982f25b968cc208751a54
Yasin Kereci <kereciyasin52@gmail.com>
2024-11-30 01:03:51 +0100

    update added

--- Project02BurgerMenu/Areas/Admin/Controllers/DashboardController.cs
+++ Project02BurgerMenu/Areas/Admin/Controllers/DashboardController.cs
@@ -1,36 +1,38 @@
-﻿using System;
+﻿using Project02BurgerMenu.Content;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Project02BurgerMenu.Entities;
-using Project02BurgerMenu.Content;
 
 namespace Project02BurgerMenu.Areas.Admin.Controllers
 {
     public class DashboardController : Controller
     {
-        BurgerMenu01Context db = new BurgerMenu01Context();
+        BurgerMenu01Context context = new BurgerMenu01Context();
         public ActionResult Index()
         {
             ViewBag.datetime = DateTime.Now.ToString("dd/MM/yyyy");
-            ViewBag.products = db.Products.Count();
-            ViewBag.reservations = db.Reservations.Count(); 
-            ViewBag.admins = db.Admins.Count();
-            ViewBag.testimonials = db.Testimonials.Count(); 
-            ViewBag.productNames = db.Products.Select(x => x.ProductName).ToList();
-            ViewBag.productPrices = db.Products.Select(x => x.ProductPrice).ToList();
-            ViewBag.productCategories = db.Products.Select(x => x.Category.CategoryName).ToList();
+            ViewBag.products = context.Products.Count();
+            ViewBag.reservations = context.Reservations.Count();
+            ViewBag.admins = context.Admins.Count();
+            ViewBag.testimonials = context.Contacts.Count();
+            ViewBag.productNames = context.Products.Select(p => p.ProductName).ToList();
+            ViewBag.productPrices = context.Products.Select(p => p.ProductPrice).ToList();
+            ViewBag.productCategory = context.Products.Select(p => p.Category.CategoryName).ToList();
 
-            Random rnd = new Random();
-            List<int> ints = new List<int>();
+            // Progress için rastgele değerler oluşturma
+            Random random = new Random();
+            List<int> progressValues = new List<int>();
+
             for (int i = 0; i < ViewBag.productNames.Count; i++)
             {
-                int randomProg = rnd.Next(0, 101);
-                ints.Add(randomProg);
-                
+                int randomProgress = random.Next(0, 101); // 0 ile 100 arasında rastgele bir sayı oluştur
+                progressValues.Add(randomProgress);
             }
-            ViewBag.ints = ints;
+
+            ViewBag.productProgress = progressValues;
+
             return View();
         }
     }
